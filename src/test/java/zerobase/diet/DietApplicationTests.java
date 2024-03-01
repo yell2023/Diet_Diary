ecd70f0a325334f909376d18bddf39cadb3a48b6
Yang ye lin <oky07031217@gmail.com>
2024-03-01 16:30:37 +0900

    Initial commit

---
+++ src/test/java/zerobase/diet/DietApplicationTests.java
@@ -0,0 +1,13 @@
+package zerobase.diet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DietApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
